6eca3987661d4073e73bc1eb472cd8fcd518cc04
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-08-21 23:16:16 +0530

    ::help for csv::

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,10 @@ module.exports = async (msg, MessageEmbed) => {
             { name: '`!r-1-5` **- shows you questions from index "1" to "5" (inclusive)**', value: 'Alternative: *range-1-5*' },
             { name: '`!on` **- lists all the active users**', value: 'Alternative: *online*' },
             { name: '`!hide` **- hides you from the list of active user, even if you are active**', value: 'Alternative: *none*' },
-            { name: '`!h` **- shows you this guide**', value: 'Alternatives: *help* *g* *guide*' }
+            { name: '`!h` **- shows you this guide**', value: 'Alternatives: *help*, *g*, *guide*' },
+            { name: '`!csv-{start}-{end}` **- sends you a csv file of *n list***', value: 'Alternatives: *none*' },
+            { name: '`!cact` **- shows no. of active csv conversions *(max: 5)***', value: 'Alternatives: *cactive*' },
+            { name: '`!csvcd` **- shows cooldown for the csv conversion command**', value: 'Alternatives: *none*' },
         );
     msg.reply({
         embeds: [embed],
