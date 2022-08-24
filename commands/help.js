1be72c96ceb4b834bc7614efd58031f4cd9d98f9
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-08-24 22:20:51 +0530

    ::help_update::

--- commands/help.js
+++ commands/help.js
@@ -3,15 +3,16 @@ module.exports = async (msg, MessageEmbed, MessageActionRow, MessageButton) => {
         .setTitle('Botto Guide📙')
         .setDescription('Bot will automatically help you with your **missions** and **reports**')
         .addFields(
-            { name: '`!s` **- shows you all the questions stored**', value: 'Alternative: *show*' },
-            { name: '`!f-t-Team 8` **- shows you all the questions containing "*Team 8"***', value: 'Alternative: *find-text-Team 8*' },
-            { name: '`!r-1-5` **- shows you questions from index "1" to "5" (inclusive)**', value: 'Alternative: *range-1-5*' },
             { name: '`!on` **- lists all the active users**', value: 'Alternative: *online*' },
-            { name: '`!hide` **- hides you from the list of active user, even if you are active**', value: 'Alternative: *none*' },
-            { name: '`!h` **- shows you this guide**', value: 'Alternatives: *help*, *g*, *guide*' },
+            { name: '`!hide` **- hides you from the list of active users, even if you are active**', value: 'Alternative: *none*' },
             { name: '`!csv-1-7` **- sends you a csv file of *n list***', value: 'Alternatives: *none*' },
             { name: '`!cact` **- shows no. of active csv conversions *(max: 5)***', value: 'Alternatives: *cactive*' },
             { name: '`!csvcd` **- shows cooldown for the csv conversion command**', value: 'Alternatives: *none*' },
+            { name: '==========================', value: '** **' },
+            { name: '`!s` **- shows you all the questions stored**', value: 'Alternative: *show*' },
+            { name: '`!f-t-Team 8` **- shows you all the questions containing "*Team 8"***', value: 'Alternative: *find-text-Team 8*' },
+            { name: '`!r-1-5` **- shows you questions from index "1" to "5" (inclusive)**', value: 'Alternative: *range-1-5*' },
+            { name: '`!h` **- shows you this guide**', value: 'Alternatives: *help*, *g*, *guide*' },
         );
 
     const inviteAndUpvote = new MessageActionRow()
